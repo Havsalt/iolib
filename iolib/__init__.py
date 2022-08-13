ed86b64c56ab4c021bea2af0e8e4189f7bb68231
FloatingInt <77575424+Floating-Int@users.noreply.github.com>
2022-08-13 14:55:22 +0200

    Added module docstring, __author__, and __version__

--- iolib/__init__.py
+++ iolib/__init__.py
@@ -1,1 +1,26 @@
+"""
+IO library inspired by the builtin function 'input'
+* selection
+* input_write
+* input_hidden
+
+Also contains decorator 'overload'
+- Used to define a function multiple times
+- Distinguishes by looking at argument types
+- Return types does not make it distinguisable
+
+Latest supported version of Python: 3.8.2
+"""
+
+__version__ = "0.3.5"
+__author__ = "FloatingInt"
+
+__all__ = [
+    "selection",
+    "input_write",
+    "input_hidden",
+    "OverloadUnmatched",
+    "overload"
+]
+
 from .main import *
